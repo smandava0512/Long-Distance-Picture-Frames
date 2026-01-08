f0f1c683059d4e895e056f744eff5e54fca134cb
shrav <smandava1@ufl.edu>
2026-01-08 15:51:58 -0500

    djlsg

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,33 @@ function App() {
       </div>
 
       <div>
-        <button className="frame-button">Home Frame</button>
-        <button className="frame-button">Remote Frame</button>
+        
+        <button className="frame-button"
+          style={{
+            border: 'none',
+            padding: 0,
+            background: 'none',
+            cursor: 'pointer',
+          }}
+        >
+          <img>
+            src={'./assets/blankFrame.jpg'}
+          </img>
+        </button>
+
+        <button className="frame-button"
+          style={{
+            border: 'none',
+            padding: 0,
+            background: 'none',
+            cursor: 'pointer',
+          }}
+        >
+          <img>
+            src={'./assets/blankFrame.jpg'}
+          </img>
+        </button>
+
       </div>
 
       </div>
