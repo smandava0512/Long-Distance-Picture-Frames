e6277774b1a4ac53028e76895ac80a965d59cb9b
shrav <smandava1@ufl.edu>
2026-01-14 13:51:50 -0500

    fixed exposeInMainWorld (added preload.js)

--- public/electron.js
+++ public/electron.js
@@ -1,15 +1,6 @@
 const {app, BrowserWindow} = require('electron');
 const url = require('url');
 const path = require('path');
-const { ipcRenderer, contextBridge } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  ipcRenderer: {
-    send: (channel, data) => ipcRenderer.send(channel, data),
-    on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
-  },
-});
-
 
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
@@ -23,9 +14,8 @@ function createMainWindow() {
         protocol: 'file:',
         slashes: true
     });
-
+  
     mainWindow.setMenuBarVisibility(false);
-
     mainWindow.loadURL(startUrl);
 
 }
